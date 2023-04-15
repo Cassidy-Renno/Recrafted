b279548f9891f50c031a959798c1272054a2e5bb
The-Math-Maniac1 <62885717+The-Math-Maniac1@users.noreply.github.com>
2023-04-15 14:47:35 -0400

    Added //comment!

--- src/main/java/net/cassidy/recrafted/Recrafted.java
+++ src/main/java/net/cassidy/recrafted/Recrafted.java
@@ -14,9 +14,8 @@ import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(Recrafted.MOD_ID)
-public class Recrafted
-{
-    // Define mod id in a common place for everything to reference
+public class Recrafted {
+    //comment!
     public static final String MOD_ID = "recrafted";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
